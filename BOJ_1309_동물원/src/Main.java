8bacaafc6663a13b85cff0f9d3888fb2b8e9747a
qsunki <ajsthfldu@outlook.com>
2024-06-24 22:10:44 +0900

    BOJ 1309: 동물원

---
+++ BOJ_1309_동물원/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int n;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        dp = new int[2];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 0; i < n; i++) {
+            int dp0 = dp[0];
+            int dp1 = dp[1];
+            dp[0] = (dp0 + dp1) % 9901;
+            dp[1] = (dp0 * 2 + dp1) % 9901;
+        }
+
+        System.out.println(dp[0]);
+    }
+}
