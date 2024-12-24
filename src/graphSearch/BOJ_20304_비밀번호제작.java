d35f68c62e129f958a2fc7cf4e79e98f8841e105
qsunki <ajsthfldu@outlook.com>
2024-12-24 10:25:53 +0900

    BOJ 20304: 비밀번호 제작

---
+++ src/graphSearch/BOJ_20304_비밀번호제작.java
@@ -0,0 +1,57 @@
+package graphSearch;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_20304_비밀번호제작 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        Queue<Node> queue = new ArrayDeque<>();
+        st = new StringTokenizer(br.readLine());
+        int[] minDistances = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            int password = Integer.parseInt(st.nextToken());
+            queue.offer(new Node(password, 0));
+            minDistances[password] = 0;
+        }
+        Arrays.fill(minDistances, Integer.MAX_VALUE);
+        int maxBit = Integer.toBinaryString(n).length();
+        bfs(queue, n, minDistances, maxBit);
+        int max = 0;
+        for (int i = 0; i <= n; i++) {
+            max = Math.max(max, minDistances[i]);
+        }
+
+        System.out.println(max);
+    }
+
+    static void bfs(Queue<Node> queue, int n, int[] minDistances, int maxBit) {
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            int nDistance = cur.distance + 1;
+            for (int i = 0; i <= maxBit; ++i) {
+                int neighbor = cur.value ^ (1 << i);
+                if (neighbor > n || nDistance >= minDistances[neighbor]) {
+                    continue;
+                }
+                minDistances[neighbor] = nDistance;
+                queue.offer(new Node(neighbor, nDistance));
+            }
+        }
+    }
+
+    static class Node {
+        int value;
+        int distance;
+
+        public Node(int value, int distance) {
+            this.value = value;
+            this.distance = distance;
+        }
+    }
+
+}
